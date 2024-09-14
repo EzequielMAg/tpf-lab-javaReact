fd604e3b3b41d08b1475399d39ee56bdae4bbbc9
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2024-09-14 20:12:55 -0300

    Update various global CSS files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
+import { useAuth } from './hooks'
 import { HomePage, LoginPage } from './pages'
-
+// import LoginPage from './pages/LoginPage/LoginPage';
 function App() {
+
+  const { isLoggedIn, login, logout } = useAuth();
 
   return (
     <>
-      <LoginPage />
-      {/* <HomePage /> */}
+      {!isLoggedIn ? (
+        <LoginPage onLogin={ login } />
+       ) : (
+        <h1>ESTOY LOGUEADO!</h1>
+        // <HomePage />
+       )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;

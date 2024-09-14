eb452a0962c7a822d19d7c51fcea8436c530886c
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2024-09-14 04:56:24 -0300

    Add useForm custom hook for form handling

---
+++ src/hooks/useForm.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+const useForm = ( initialForm = {} ) => {
+
+  const [formState, setFormState] = useState( initialForm );
+
+  const onInputChange = ( event ) => {
+    const { name, value } = event.target;
+    setFormState((prevState) => ({ ...prevState, [name]: value }));
+    console.log(name, value);
+  }
+
+  const onResetForm = () => {
+    setFormState( initialForm );
+  }
+
+  const handleSubmit = (callback) => {
+    return (event) => {
+      event.preventDefault()
+      callback();
+      console.log(formState);
+    };
+  };
+
+  return { 
+    ...formState,
+    formState,
+    onInputChange,
+    onResetForm,
+    handleSubmit
+  }
+}
+
+export default useForm;

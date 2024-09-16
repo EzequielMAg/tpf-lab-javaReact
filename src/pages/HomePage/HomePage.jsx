9a2ccb597025caeb37bdf9b0759072407e7158bc
Ezequiel Mamani Aguilar <ezequiel.m.aguilar@gmail.com>
2024-09-15 22:45:05 -0300

    Add initial version of HomePage component with updated styles

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,16 +1,30 @@
-"use client";
-import React from 'react';
-import './HomePage.css';
-import PropTypes from 'prop-types';
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../../hooks";
+import styles from "./HomePage.module.css";
 
-const HomePage = ({}) => {
-	return (
-		<div className='homepage'>
- 			HomePage works!
- 		</div>
-	);
+const HomePage = ({ }) => {
+
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+  const onLogout = () => {
+    logout();
+    navigate("/");
+  }
+
+  return (
+    <div className={ styles['home-page'] }>
+      <h2>HomePage works!</h2>
+      <button
+        type="button"
+        className={ styles.btn }
+        onClick={ onLogout }
+        title="Esto es un boton que hereda estilos"
+      >
+        volver
+      </button>
+    </div>
+  );
 };
-
-HomePage.propTypes = {};
 
 export default HomePage;
